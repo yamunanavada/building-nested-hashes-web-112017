ec8481e3b3b6f8b4543651925fe328b883e87fb6
Yamuna Navada <yamunanavada@gmail.com>
2017-11-04 15:03:24 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet", :age => "50"
@@ -26,5 +26,5 @@ def third_challenge
    }
   }
 
-  
+
 end
