ab840ebc476d39710ff17001867edb9388a8620d
Yamuna Navada <yamunanavada@gmail.com>
2017-11-04 14:57:58 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,17 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
 
-      :patriarch => {}, 
+      :patriarch => {},
       :matriarch => {},
       :hero => {}
       :hero_friends => []
-      }, 
+      },
     :capulet => {
-      :patriarch => {}, 
+      :patriarch => {},
       :matriarch => {},
       :hero => {}
       :hero_friends => []
       }}
 
-  
+
 end
